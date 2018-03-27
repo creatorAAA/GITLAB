b29e3f44f7b03f261ca00409a7f907a0b5e7aa35
creatorAAA <panithan.tuppasarndumrong@gmail.com>
2018-03-27 14:02:34 +0700

    change PrintGrade.c 2 lines

--- PrintGrade.c
+++ PrintGrade.c
@@ -2,8 +2,8 @@
 void main()
 {
   char Name[30][40] = {"John Wick", " Mike Miller", "Ying Phakpon","Chai Chanchai", "Yong Boonsith", "Kitti Karndee"}; 
-  int ID[30] = {5301, 5302, 5303, 5304, 5305, 5306}; 
-  float GPA[30] = {2.73, 2.35, 3.65, 3.24, 1.32, 3.26}; 
+  int ID[30] = {9000, 2500, 5303, 5304, 5305, 5306}; 
+  float GPA[30] = {4.20, 3.22, 5.55, 3.24, 1.32, 3.26}; 
   int N = 6, i;
   int SearchID, found_index; 
  
